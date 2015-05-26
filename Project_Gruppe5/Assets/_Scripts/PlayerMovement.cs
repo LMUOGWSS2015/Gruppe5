67486b3bc51ec40814ccfee642c7a5d88d50e27e
Dominik Seeger <seeger.dominik@googlemail.com>
2015-05-26 18:50:13 +0200

    Controller (Xbox360) für Win und Mac implementiert (bislang nur linker Stick) - Auswahl im Player Movement Script, ob auf Win oder Mac gespielt wird

--- Project_Gruppe5/Assets/_Scripts/PlayerMovement.cs
+++ Project_Gruppe5/Assets/_Scripts/PlayerMovement.cs
@@ -4,10 +4,29 @@ using System.Collections;
 public class PlayerMovement: MonoBehaviour {
 
 	public float speed = 3.0f;
+	public bool playWithControllerMac = false;
+	public bool playWithControllerWin = false;
 
 	void FixedUpdate() {
-		float translation = Input.GetAxis("Vertical") * speed;
-		float translationH = Input.GetAxis("Horizontal") * speed;
+
+		float translation;
+		float translationH;
+
+		if (playWithControllerMac) {
+			translation = Input.GetAxis ("XboxMacLeftY") * speed;
+			translationH = Input.GetAxis ("XboxMacLeftX") * speed;
+
+		}
+		else if (playWithControllerWin) {
+			translation = Input.GetAxis ("XboxWinLeftY") * speed;
+			translationH = Input.GetAxis ("XboxWinLeftX") * speed;
+			
+		}
+		else {
+			translation = Input.GetAxis ("Vertical") * speed;
+			translationH = Input.GetAxis ("Horizontal") * speed;
+		}
+
 		translation *= Time.deltaTime;
 		translationH *= Time.deltaTime;
 		transform.Translate(translationH, 0, translation);
