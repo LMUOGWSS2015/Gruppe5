f377932d503457541526b61ebb13a16ddcfbf401
Annika Busch <annika.m.busch@gmail.com>
2015-06-04 17:51:59 +0200

    Charge-Script;   Gegner, die nur drehen, nicht laufen;

---
+++ Project_Gruppe5/Assets/_Scripts/Charge.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Charge : MonoBehaviour {
+	public float timeToChargeInSec = 10f;
+	public float chargePercentage = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
