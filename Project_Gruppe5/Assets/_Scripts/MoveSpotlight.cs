4059f31b28da4296aed9acc86cdb0356e643750f
Alexander Klimczak <alexander.klimczak@gmail.com>
2015-06-08 15:28:43 +0200

    Lichter funktionieren, Eyetracking eingebaut.

--- Project_Gruppe5/Assets/_Scripts/MoveSpotlight.cs
+++ Project_Gruppe5/Assets/_Scripts/MoveSpotlight.cs
@@ -1,20 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
+using iView;
 
 public class MoveSpotlight : MonoBehaviour
 {
+	public float speed = 2f;
+	public float stagewidth = 40f;
+	public float stageheight = 20f;
+	private Collider plane;
 	Vector3 newPosition;
-
+	Vector3 dPosition;
+	SampleData sample;
+	Vector3 averageGazePosition;
+	float diagonallength;
 	void Start () {
+		Vector3 diagonal= new Vector3(stagewidth,0,stageheight);
+		diagonallength=Vector3.Magnitude(diagonal);
 		newPosition = transform.position;
+		plane = GameObject.Find ("Floor").GetComponent<Collider>();
 	}
-
+	
 	void Update() {
+		
+		//get the Sample from the Server
+		sample = SMIGazeController.Instance.GetSample();
+		//get the averaged GazePosition
+		averageGazePosition = sample.averagedEye.gazePosInUnityScreenCoords();
+		
+		Ray ray;
+		
+		if (averageGazePosition.x==0f) 
+			ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		else
+			ray = Camera.main.ScreenPointToRay (averageGazePosition);
 		RaycastHit hit;
-		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		if (Physics.Raycast(ray, out hit)) {
+		//float hit;
+		if (plane.Raycast(ray, out hit,100f)) {
+			//Vector3 hitpoint = ray.GetPoint(hit);
+			//newPosition = new Vector3(hitpoint.x,5f,hitpoint.z);
 			newPosition = new Vector3(hit.point.x, 5f, hit.point.z);
-			transform.position = newPosition;
+			Debug.Log (" m:"+ Input.mousePosition+"position:"+newPosition);
+			Vector3 to = newPosition-transform.position;
+			float length= Vector3.Magnitude(to);
+			to = to.normalized;
+			transform.position = transform.position+(to*speed*length/diagonallength);
 		}
 	}
 }
