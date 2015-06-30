2838984261246163af5739748c68b5dd990008b8
Dominik Seeger <seeger.dominik@googlemail.com>
2015-06-30 16:10:35 +0200

    Fernseher eingebaut

--- Project_Gruppe5/Assets/_Scripts/Intro/IntroController.cs
+++ Project_Gruppe5/Assets/_Scripts/Intro/IntroController.cs
@@ -2,14 +2,46 @@
 using System.Collections;
 
 public class IntroController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
 	
+	public GameObject playerObject;
+	public float stepSpeed;
+	private bool movePlayer = false;
+	private Vector3 targetPos = new Vector3 (0f, 0.55f, -6f);
+	private float step;
+	private IntroDDoorsLowerOpen doors;
+	public GameObject doubleDoorsLower;
+	
+	IntroLevel level;
+	
+	void Start () {
+		//		doors = GameObject.FindGameObjectWithTag ("DDoorsLower").gameObject.GetComponent<DoubleDoorsLowerOpen> ();
+		doors = doubleDoorsLower.gameObject.GetComponent<IntroDDoorsLowerOpen> ();
+		//		Debug.Log (doors);
+		
+		level = transform.gameObject.GetComponent<IntroLevel> ();
+		level.StartLevel (false);
+		
+		doors.enabled = true;
+		movePlayer = true;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void Update() {
+		if (Input.GetKey (KeyCode.Escape)) {
+			Application.Quit ();
+		}
+		
+		//		if (Input.GetKeyDown (KeyCode.G)) {
+		//			doors.enabled = true;
+		//			movePlayer = true;
+		//		}
+		if (movePlayer) {
+			step = stepSpeed * Time.time;
+			//			Debug.Log(step);
+			playerObject.transform.position = Vector3.MoveTowards (playerObject.transform.position, targetPos, 0.1f);
+			if (playerObject.transform.position == targetPos) {
+				movePlayer = false;
+				doors.closeDoors();
+			}
+		}
 	}
 }
