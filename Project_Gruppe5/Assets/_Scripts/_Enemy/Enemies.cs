11a1a8d9d9a663d62678d4754778112cb1c867cd
Annika Busch <annika.m.busch@gmail.com>
2015-07-01 14:11:38 +0200

    Prefabs von Crawler, MiniTurret und Player verändert gainHeadlight room

--- Project_Gruppe5/Assets/_Scripts/_Enemy/Enemies.cs
+++ Project_Gruppe5/Assets/_Scripts/_Enemy/Enemies.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Enemies : MonoBehaviour {
 	public int size = 0;
 
-	public void Less(int amnt){
+	public virtual void Less(int amnt){
 		size -= amnt;
 		if (size <= 0)
 			GameObject.FindGameObjectWithTag ("DDoors").GetComponent<DoubleDoorsOpen> ().SetEnemiesDead(true);
