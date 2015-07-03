afdefea254db1e97558aad9cbe6d8f3a1fe168b3
Phabian <fabian.nussberger@web.de>
2015-07-03 19:42:20 +0200

    Added Events to shoot animation. Shots now triggered by the events in the shot animation.

---
+++ Project_Gruppe5/Assets/testShoot.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testShoot : MonoBehaviour {
+
+	private 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Shoot(){
+		transform.parent.gameObject.BroadcastMessage("shoot");
+
+	}
+}
