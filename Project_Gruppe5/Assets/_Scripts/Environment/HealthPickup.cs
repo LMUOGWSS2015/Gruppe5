02560f3e1ccb6f74d03da9eeecd11b5a5bb5c8e8
Annika Busch <annika.m.busch@gmail.com>
2015-07-07 17:35:43 +0200

    Pickup erst nachdem Crate zerstört wurde

--- Project_Gruppe5/Assets/_Scripts/Environment/HealthPickup.cs
+++ Project_Gruppe5/Assets/_Scripts/Environment/HealthPickup.cs
@@ -4,13 +4,14 @@ using System.Collections;
 public class HealthPickup : MonoBehaviour {
 	protected PlayerHealth playerHealth;
 	public AudioClip pickupHealthSound;
+	private bool toPick = false;
 	
 	void Awake (){
 		playerHealth = GameObject.FindGameObjectWithTag ("Player").GetComponent <PlayerHealth> ();
 	}
 	
 	void OnTriggerEnter (Collider other){
-		if(other.gameObject.tag == "Player"){
+		if(other.gameObject.tag == "Player" && toPick){
 			this.GetComponent<AudioSource>().PlayOneShot(pickupHealthSound);
 			this.GetComponent<BoxCollider>().enabled=false;
 			Behaviour halo = (Behaviour)GetComponent("Halo");
@@ -21,5 +22,9 @@ public class HealthPickup : MonoBehaviour {
 			
 			Destroy(this.gameObject,0.5f);
 		}
+	}
+
+	public void SetToPick (bool b){
+		toPick = b;
 	}
 }
--- Project_Gruppe5/Assets/_Scripts/ObjectShatter.cs
+++ Project_Gruppe5/Assets/_Scripts/ObjectShatter.cs
@@ -2,20 +2,22 @@
 using System.Collections;
 
 public class ObjectShatter : MonoBehaviour {
-	
-
 	public float explDuration = 2f;
 	public GameObject brokenCrate;
 	
 	protected ArrayList tags = new ArrayList();
+	HealthPickup hp;
 	
 	protected virtual void Awake(){
 		tags.Add ("Bullet");
 		tags.Add ("enemyBullet");
+
+		hp = this.transform.parent.GetComponentInChildren<HealthPickup> ();
 	}
 	
 	void OnTriggerEnter (Collider other) {
 		if (tags.Contains(other.gameObject.tag)) {
+			hp.SetToPick(true);
 			Destroy(Instantiate (brokenCrate,transform.position,transform.rotation),5f);
 			Destroy (this.gameObject);
 		}
