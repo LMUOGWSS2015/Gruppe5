2c19e4a88dd57a9c2f605aca4eb63d4eaf0422c2
Phabian <fabian.nussberger@web.de>
2015-07-07 15:16:57 +0200

    Added first trash-robot. Added Kameras an Ecken die Player anschauen Some Materials and Textures

---
+++ Project_Gruppe5/Assets/_Scripts/NeutralLookAt.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NeutralLookAt : MonoBehaviour {
+	/*public float distance = 8;
+	public float speed = 0.01f;
+
+	protected Transform gazeLight;
+	protected Transform player;
+	protected PlayerHealth playerHealth;
+	EnemyHealth enemyHealth;
+	Animator anim;*/
+	public float rotSpeed = 5f;
+	private Transform player;
+
+	
+	protected void Awake (){
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+	
+	}
+	
+	
+	protected void Update (){
+
+	
+
+				Rotate (player, true);
+
+	}
+	
+	protected void Rotate(Transform t, bool player){
+	
+		
+
+		
+		Vector3 dir = t.position - transform.position;
+		dir.y = 0;
+		
+		if(dir != Vector3.zero)
+			StartCoroutine(Rotation(Quaternion.LookRotation(dir), rotSpeed));
+		//		if(player ){&& Quaternion.Angle(transform.rotation, Quaternion.LookRotation(dir)) < Mathf.Epsilon){
+		//			eaf.enabled = true;
+		//		}
+		// transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), Time.time * speed);
+	}
+	
+	IEnumerator Rotation(Quaternion to, float time) {
+		float elapsedTime = 0f;
+		while (elapsedTime < time) {
+			elapsedTime += Time.deltaTime;
+			transform.rotation = Quaternion.Slerp(transform.rotation, to, elapsedTime);
+			yield return new WaitForEndOfFrame ();
+		}
+		yield return null;
+	}
+
+}
