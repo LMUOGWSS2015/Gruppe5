77e9d63d420a2de31cbcd6f9fec61ae02f8666a1
Tobias Martin <tobiasmartin@dhcp-10-181-29-215.dynamic.eduroam.mwn.de>
2015-07-08 14:33:30 +0200

    main music object for music controll, applied in intro+room1

---
+++ Project_Gruppe5/Assets/FinalRoom/MainMusic.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMusic : MonoBehaviour {
+
+	private AudioSource[] audioSource;
+
+	public GameObject player;
+	private PlayerHealth playerHealth;
+	private bool firstDeathOfScene = false;
+
+	void Start () {
+		audioSource = this.gameObject.GetComponents<AudioSource> ();
+		
+		playerHealth = player.GetComponent <PlayerHealth> ();
+
+		audioSource [0].enabled = true;
+
+		Debug.Log (audioSource [0].clip);
+		Debug.Log (audioSource [1].clip);
+		Debug.Log (playerHealth.isDead);
+	}
+	
+	void Update () {
+		if (playerHealth.isDead) {
+			firstDeathOfScene = true;
+
+			audioSource [0].Stop ();
+			audioSource [0].enabled = false;
+			
+			audioSource [1].enabled = true;
+		}
+		
+		if (!playerHealth.isDead && firstDeathOfScene) {
+			audioSource [0].enabled = true;
+			
+			audioSource [1].Stop ();
+			audioSource [1].enabled = false;
+		}
+	}
+}
